5666c58c42c15c1744b236a0961a27c9306c93aa
seon <seonu0823@gmail.com>
2019-04-01 21:11:18 +0900

    #1-1 Creating GraphQL Server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
-console.log('Hello');
+require('dotenv').config();
+
+import { GraphQLServer } from 'graphql-yoga';
+
+const PORT = process.env.PORT || 4000; 
+
+const typeDefs = `
+    type Query {
+        hello : String!
+    }
+`;
+
+const resolvers = {
+    Query : {
+        hello : () => 'Hi'
+    }
+};
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+
+server.start({ port : PORT }, () => console.log(`Server running on port http://localhost:${ PORT }`));
