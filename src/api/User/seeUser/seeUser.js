1a5c052406ad12f0f698ea5b8500eff11735cc8b
seon <seonu0823@gmail.com>
2019-04-14 15:02:43 +0900

    #3-12 me Resolver + Prisma's Limitations

--- src/api/User/seeUser/seeUser.js
+++ src/api/User/seeUser/seeUser.js
@@ -1,11 +1,12 @@
 import { prisma } from '../../../../generated/prisma-client';
+import { USER_FRAGMENT } from '../../../fragments';
 
 export default {
     Query : {
         seeUser : async (_, args) => {
             const { id } = args;
 
-            return prisma.user({ id });
+            return prisma.user({ id }).$fragment(USER_FRAGMENT);
         }
     }
 };
