0e342c6faec0f8f08e48f70aaba01bfdecc38b74
seon <seonu0823@gmail.com>
2019-04-15 20:32:34 +0900

    #3-14 Computed Fields in Prisma

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -15,5 +15,15 @@ export default {
                 posts
             };
         }
+    },
+    // Custom Resolver
+    // 하나의 field만을 위한 것.
+    // 다른 field들에게 영향이 가지 않는다.
+    // Prisma에서 먼저 찾고, 찾을 수 없다면 내 서버에서 찾는다.
+    User : {
+        // #3-14 Computed Fields in Prisma 참고
+        fullName : (parent) => {
+            return `${ parent.firstName } ${ parent.lastName }`;
+        }
     }
 };
