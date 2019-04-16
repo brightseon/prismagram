a0940002d46b4c8e7e00330ecfcddad887d4d677
seon <seonu0823@gmail.com>
2019-04-16 21:11:43 +0900

    #3-15 itsMe and amIFollowing Fileds part One

---
+++ src/api/User/computed.js
@@ -0,0 +1,50 @@
+import { prisma } from '../../../generated/prisma-client';
+
+export default {
+    // Custom Resolver
+    // 하나의 field만을 위한 것.
+    // 다른 field들에게 영향이 가지 않는다.
+    // Prisma에서 먼저 찾고, 찾을 수 없다면 내 서버에서 찾는다.
+    User : {
+        // #3-14 Computed Fields in Prisma 참고
+        fullName : parent => {
+            return `${ parent.firstName } ${ parent.lastName }`;
+        },
+        amIFollowing : async (parent, _, { request }) => {
+            const { user } = request;
+            const { id : parentId } = parent;
+
+            try {
+                const exists = await prisma.$exists.user({
+                    AND : [
+                        {
+                            id : parentId
+                        },
+                        {
+                            followers_some : [
+                                user.id
+                            ]
+                        }
+                    ]
+                });
+    
+                if(exists) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } catch(err) {
+                console.log('computed.js amIFollowing error : ', err);
+
+                return false;
+            }
+        },
+        // 요청하는 사람(parent)과 요청하는 사람(request)이 같으면, 내 프로필을 요청
+        itsMe : (parent, _, { request }) => {
+            const { user } = request;
+            const { id : parentId } = parent;
+
+            return user.id === parentId;
+        }
+    }
+};
