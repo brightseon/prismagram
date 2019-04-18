c827bee60f37e6a9661ddfc64308d3a64f782ff4
seon <seonu0823@gmail.com>
2019-04-18 20:35:04 +0900

    #3-17 isLiked Computed File

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -40,5 +40,26 @@ export default {
 
             return user.id === parentId;
         }
+    },
+    Post : {
+        isLiked : (parent, _, { request }) => {
+            const { user } = request;
+            const { id } = parent;
+
+            return prisma.$exists.like({
+                AND : [
+                    {
+                        user : {
+                            id : user.id
+                        }
+                    },
+                    {
+                        post : {
+                            id
+                        }
+                    }
+                ]
+            });
+        }
     }
 };
