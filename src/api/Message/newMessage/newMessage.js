b6e81a7d2e3aa3371d8a1537384136994231a6a3
seon <seonu0823@gmail.com>
2019-04-26 22:11:29 +0900

    #3-28 newMessage Subscription part Two

---
+++ src/api/Message/newMessage/newMessage.js
@@ -0,0 +1,24 @@
+import { prisma } from '../../../../generated/prisma-client';
+
+export default {
+    Subscription : {
+        newMessage : {
+            subscribe : (_, args) => {
+                const { roomId } = args;
+
+                return prisma.$subscribe.message({
+                    AND : [
+                        { mutation_in : 'CREATED' },
+                        {
+                            node : {
+                                room : { id : roomId }
+                            }
+                        }
+                    ]
+                });
+            },
+            // #3-28. newMessage Subscription part Two 참고
+            resolve : payload => payload
+        }
+    }
+};
