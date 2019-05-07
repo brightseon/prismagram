2206a1fb5311978c75e045ee04fbd7480f1a5b1c
seon <seonu0823@gmail.com>
2019-05-07 21:50:23 +0900

    #6-2 Header Logic part Two

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -7,13 +7,7 @@ export default {
 
             const { user } = request;
 
-            const userProfile = await prisma.user({ id : user.id });
-            const posts = await prisma.user({ id : user.id }).posts();
-
-            return {
-                user : userProfile,
-                posts
-            };
+            return await prisma.user({ id : user.id });
         }
     }
 };
